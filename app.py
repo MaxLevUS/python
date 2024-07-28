96dfbb05b779d0c7422e8678e0ebb39771fe8e5e
Maksim Levanovich <maxlevanovich@gmail.com>
2024-07-28 08:49:57 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,94 @@
+# print("Maksim Hello")
+# my_list = [1, 2, 3]
+
+
+# print(my_list)
+
+
+"""long_str = "This is a 
+very 
+long string"
+
+print("1) " + long_str)
+
+print("2) ", type(long_str))
+
+print("3) ", len(long_str))
+
+print("4) ", long_str[0])
+
+print("5) " + long_str[0:6])
+
+print("6) " + long_str[3:])"""
+
+
+# Methods
+
+"""my_new_string = "Hello, My name is Maksim"
+
+print(my_new_string)
+
+print(my_new_string.replace("Maksim", "Anastasiya"))
+
+print(my_new_string.count(" "))
+
+print(my_new_string.count("is"))"""
+
+
+# Целые числа
+
+
+"""friend_qty = 50
+
+print(friend_qty)
+
+print(type(friend_qty))
+
+
+any_num = input("Enter any number: ")
+
+print(any_num)
+
+print(type(any_num))  # type will be - str
+
+
+int_num = int(input("Insert any number: "))
+
+print(int_num)
+
+print(type(int_num))  # type will be - number"""
+
+
+# Возведение в степень
+
+base = 5
+power = 3
+
+print(pow(base, power))
+
+
+# Длинные числа - разрешены нижние подчеркивания, для удобства чтения, но при выводе они не учитываются
+
+"""one_million = 1_000_000
+
+print(one_million)  # 1000000
+
+my_number = 3_427
+
+print(my_number)  # 3427"""
+
+# Задача
+
+"""input_str = input("Insert any number  ")
+input_int = int(input_str)
+
+print(input_int)
+
+print(type(input_int))"""
+
+
+first_num = 5
+second_num = 10
+
+print(pow(first_num, second_num))
+print(type(pow(first_num, second_num)))
